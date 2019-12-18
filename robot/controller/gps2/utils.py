8933f88b51708a3b12197c8b21637dcc90537674
hzaskywalker@gmail.com <hzaskywalker@gmail.com>
2019-12-17 21:41:57 -0800

    gps

---
+++ robot/controller/gps2/utils.py
@@ -0,0 +1,12 @@
+def check_shape(value, expected_shape, name=''):
+    """
+    Throws a ValueError if value.shape != expected_shape.
+    Args:
+        value: Matrix to shape check.
+        expected_shape: A tuple or list of integers.
+        name: An optional name to add to the exception message.
+    """
+    if value.shape != tuple(expected_shape):
+        raise ValueError('Shape mismatch %s: Expected %s, got %s' %
+                         (name, str(expected_shape), str(value.shape)))
+
