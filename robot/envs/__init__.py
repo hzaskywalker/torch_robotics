d354500915e1a28ae1c923e972f79f40e02bce8e
hzaskywalker@gmail.com <hzaskywalker@gmail.com>
2020-01-04 13:42:38 -0800

    dm control...

--- robot/envs/__init__.py
+++ robot/envs/__init__.py
@@ -1,12 +1,1 @@
-import gym
-from .gym_wrapper import CartPoleWrapper
-from .dm_control import make as dm_make
 
-def make(env_name, mode='gym'):
-    if env_name == 'CartPole-v0':
-        if mode == 'gym':
-            return CartPoleWrapper(gym.make(env_name))
-        elif mode == 'dm_control':
-            return dm_make('cartpole', task_name='balance')
-    else:
-        raise NotImplementedError
