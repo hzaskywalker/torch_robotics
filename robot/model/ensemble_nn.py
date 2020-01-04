fix some normalization error

robot/model/ensemble_nn.py
@@ -161,20 +161,24 @@ class EnBNNAgent(AgentBase):
                 s = t
             return torch.stack(outs, dim=2), rewards.reshape(self.ensemble_size, -1, a.shape[0]).mean(dim=(0, 1))
 
-    def fit_normalizer(self, data_gen):
-        # very strange function
-        idx = 0
-        for s, a, _ in data_gen:
-            s = self.state_prior.encode(s)
-            if idx == 0:
-                self.obs_norm.fit(s)
-                self.action_norm.fit(a)
-                idx = 1
-            else:
-                self.obs_norm.update(s)
-                self.action_norm.update(a)
-        print('normalizer')
-        print(self.obs_norm.mean, self.obs_norm.std, self.obs_norm.count)
+    def fit_normalizer(self, buffer):
+        # TODO: very very ugly
+        if self.normalizer:
+            print('fit normalizer...')
+            data_gen = buffer.make_sampler('fix', 'train', 1, use_tqdm=False)
+            # very strange function
+            idx = 0
+            for s, a, _ in data_gen:
+                s = self.state_prior.encode(s)
+                if idx == 0:
+                    self.obs_norm.fit(s)
+                    self.action_norm.fit(a)
+                    idx = 1
+                else:
+                    self.obs_norm.update(s)
+                    self.action_norm.update(a)
+            print('normalizer')
+            print(self.obs_norm.mean, self.obs_norm.std, self.obs_norm.count)
 
     def update(self, s, a, t):
         if self.training:

robot/utils/normalizer.py
@@ -44,7 +44,7 @@ class Normalizer(nn.Module):
         # calculate the new mean and std
         self.mean[:] = self.sum / self.count
         std = ((self.sumsq/self.count) - self.mean **2)
-        self.std[:] = std.clamp(self.eps**2, float('inf'))
+        self.std[:] = torch.sqrt(std.clamp(self.eps**2, float('inf')))
 
     # normalize the observation
     def norm(self, v: torch.Tensor, clip_range=None):

workspace/pets.py
@@ -53,7 +53,7 @@ def test_env():
 
 def load_parameters(model: EnBNNAgent):
     import torch
-    xx = torch.load('/home/hza/handful-of-trials-pytorch/model_cheetah.t')
+    xx = torch.load('/home/hza/handful-of-trials-pytorch/xx')
     idx = 0
     for i in model.forward_model.parameters():
         while xx[idx].shape[-1] == 24: #6:
@@ -143,7 +143,8 @@ def test_halfcheetah():
         ensemble_size=5,
         num_layers=5,
         mid_channels=200,
-        normalizer=False,
+        normalizer=True,
+        npart=20,
     ).cuda()
 
     controller = RolloutCEM(
@@ -160,7 +161,9 @@ def test_halfcheetah():
     )
 
     if False:
-        load_parameters(model)
+        import torch
+        with torch.no_grad():
+            load_parameters(model)
 
         """
         obs = env.reset()
@@ -192,15 +195,12 @@ def test_halfcheetah():
         vis = Visualizer('/tmp/halfcheetah/history')
     )
 
-    #mb_controller.test(env, use_tqdm=True, print_reward=True)
-    #exit(0)
-
     mb_controller.init(env)
     for it in range(200):
         print(it, mb_controller.fit(env, progress_buffer_update=False, progress_rollout=True,
                                     progress_train=True, num_train=5))
 
 if __name__ == '__main__':
-    test_cartpole()
+    #test_cartpole()
     #test_env()
-    #test_halfcheetah()
+    test_halfcheetah()