769a5877f5f64e03a3177a82f96a5546576b5f60
hzaskywalker@gmail.com <hzaskywalker@gmail.com>
2020-01-21 11:16:49 -0800

    new array and space

---
+++ robot/envs/frame/space.py
@@ -0,0 +1,131 @@
+import numpy as np
+import torch
+from collections import OrderedDict
+from gym.spaces import Space as GymSpace
+from .utils import to_numpy, to_tensor, serialize, deserialize
+
+
+class Space(GymSpace):
+    def __init__(self, shape, dtype):
+        # property of leaf space
+        import numpy as np  # takes about 300-400ms to import, so we load lazily
+        self._size = None
+        self._shape = shape
+        self.dtype = None if dtype is None else np.dtype(dtype)
+        self.np_random = None
+        self.seed()
+
+    @property
+    def size(self):
+        # return the size of variable in this space
+        if self._size is None:
+            self._size = int(np.prod(self.shape))
+        return self._size
+
+    @property
+    def shape(self):
+        # return the shape of variable in this space
+        return self._shape
+
+    def serialize(self, state, is_batch=False):
+        # return the serialization
+        return serialize(state, is_batch)
+
+    def deserialize(self, vector, is_batch=False):
+        # return the de-serialization
+        return deserialize(vector, self.shape, is_batch)
+
+    def id(self, state, index):
+        #return Frame(self.state[index], isinstance(index, tuple))
+        if isinstance(state, np.ndarray) or isinstance(state, torch.Tensor):
+            return state[index]
+        else:
+            return OrderedDict([(i, self.id(v, index)) for i, v in state.items()])
+
+    def to_numpy(self, state):
+        # return the same type object, while the data is numpy
+        return to_numpy(state)
+
+    def to_tensor(self, state, device='cuda:0'):
+        # return the same type object, while the data is saved as pytorch Tensor
+        return to_tensor(state, device)
+
+    # main function to override for the previous tasks...
+    def contains(self, x):
+        raise NotImplementedError
+
+    def sample(self):
+        raise NotImplementedError
+
+    # main function to override for new
+    def observe(self, state, scene=None):
+        raise NotImplementedError
+
+    def sub(self, a, b, scene=None):
+        raise NotImplementedError
+
+    def add(self, a, b, scene=None):
+        raise NotImplementedError
+
+    def metric(self, a, scene=None, is_batch=False):
+        raise NotImplementedError
+
+    @property
+    def derivative_space(self):
+        # the deriviative space will be the same as observation_space by default
+        return self.observation_space
+
+    @property
+    def observation_space(self):
+        # the observation space will be the same to self by default
+        return self
+
+    def __repr__(self):
+        return "Space("+str(self.shape)+")"
+
+    def __call__(self, state, is_batch, scene=None):
+        # return a Frame variable that help us to write the code...
+        return Frame(self, state, scene, is_batch)
+
+
+class Frame:
+    def __init__(self, space: Space, state, scene=None, is_batch=False):
+        self.space = space
+        self.scene = scene
+        self.state = state
+        self.is_batch = is_batch
+
+    def __array__(self):
+        return self.space.serialize(self.numpy().observe(), self.is_batch) # must be numpy
+
+    def observe(self):
+        return self.space.observe(self.state, self.scene)
+
+    def numpy(self):
+        scene = None if self.scene is None else to_numpy(self.scene)
+        return Frame(self.space, to_numpy(self.state), scene, self.is_batch)
+
+    def tensor(self, device):
+        scene = None if self.scene is None else to_tensor(self.scene, device)
+        return Frame(self.space, to_tensor(self.state, device), scene, self.is_batch)
+
+    def __sub__(self, other):
+        if isinstance(other, Frame):
+            other = other.state
+        return self.space.sub(self.state, other, self.scene)
+
+    def __add__(self, other):
+        if isinstance(other, Frame):
+            other = other.state
+        return self.space.add(self.state, other, self.scene)
+
+    def metric(self):
+        return self.space.metric(self.state, self.scene, self.is_batch)
+
+    def id(self, index):
+        assert self.is_batch
+        return Frame(self.scene.id(self.state, index), isinstance(index, tuple))
+
+    def __repr__(self):
+        return "Frame("+str(self.state)+") of "+str(self.space)
+
---
+++ robot/envs/frame/test/test_array.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch
+from robot.envs.frame.array import Array
+from robot.envs.frame.space import Frame
+
+def check_np_array(a, b):
+    return ((a-b)**2).sum() < 1e-6
+
+def test_array_space():
+    space = Array(np.array([1, 1, 1]))
+    a = space(space.sample(), False)
+    assert isinstance(a, Frame)
+
+    # numpy
+    out = a.state
+    assert check_np_array(np.array(a), out), "numpy error"
+    assert check_np_array(np.array(a.tensor('cpu')),  out), "cpu tensor error"
+    assert check_np_array(np.array(a.tensor('cuda:0')),  out), "cuda:0 tensor error"
+
+    # cuda
+    a = a.tensor('cuda:0')
+    assert isinstance(a.state, torch.Tensor)
+    assert check_np_array(np.array(a), out) #cuda
+
+    # operator
+    a = space(np.array([1, 2, 3, 5]), False)
+    b = space(np.array([2, 3, 4, 9]), False)
+
+    assert check_np_array(np.array(a+b), np.array([3, 5, 7, 14]))
+    assert check_np_array(np.array(a-b), np.array([-1, -1, -1, -4]))
+    assert np.abs(a.metric() - (1**2 + 2**2 + 3**2 + 5**2)) < 1e-5
+
+    # batch version
+    a = space(np.array([[1, 2, 3], [2, 3, 4]]), is_batch=True)
+    assert check_np_array(a.metric(), np.array([1**2 + 2**2 + 3**2, 2**2 + 3** 2 + 4**2]))
+
+    # in
+    assert np.array([0.1, 0.1, 0.1]) in space
+    assert np.array([-0.1, -0.1, -0.1]) in space
+    assert not np.array([-1.1, -0.1, -0.1]) in space
+
+
+if __name__ == '__main__':
+    test_array_space()
