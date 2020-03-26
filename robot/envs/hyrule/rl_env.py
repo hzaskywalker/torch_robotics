fix jacobian gradient, upload full package

robot/envs/hyrule/rl_env.py
@@ -149,7 +149,7 @@ class ArmReach(Simulator):
 
     def _get_obs(self):
         # pass
-        observation = np.concatenate([self.agent.get_qpos(), self.agent.get_qvel()])
+        observation = np.concatenate([self.agent.get_qpos(), self.agent.get_qvel(), self.agent.get_qacc()])
 
         ee_idx = self._ee_link_idx['agent']
         achieved_goal = self.agent.get_links()[ee_idx].pose.p

robot/model/arm/dataset.py
@@ -38,12 +38,13 @@ def data_collector(env, make_policy, num_episode, timestep, path, make=None, use
                 if geom is not None:
                     geoms = np.empty([num_episode, timestep, geom.shape[-1]])
 
-            observations[i, j] = obs
-            actions[i, j] = action
-
             if geom is not None:
                 geoms[i, j] = geom
 
+            
+            # action from PREVIOUS timestep matches current obs
+            observations[i, j] = obs
+            actions[i, j] = action
             obs, reward, done, _ = env.step(action)
             if done:
                 break
@@ -58,7 +59,7 @@ def make_dataset(env_name):
     from robot.model.arm.envs.arm_controller import RandomController, Controller as ArmController
     from robot.model.arm.envs.test_acrobat import AcrobatController
 
-    path = os.path.join('/dataset/', env_name)
+    path = os.path.join('./dataset/', env_name)
     Controller = {
         'arm': ArmController,
         'acrobat2': AcrobatController,
@@ -108,7 +109,7 @@ class Dataset:
             b = int(i.split('/')[-1].split('.')[0])
             #if b != 0:
             #    continue
-            with open(os.path.join(path, i), 'rb') as f:
+            with open(i, 'rb') as f:
                 data = pickle.load(f)
                 observations.append(data[0])
                 actions.append(data[1])
@@ -164,7 +165,7 @@ class Dataset:
 
 def test():
     from robot.model.arm.envs import make
-    dataset = Dataset('/dataset/plane')
+    dataset = Dataset('./dataset/plane')
     # test acrobat
     s = dataset.sample(batch_size=1, timestep=25)[0][0].detach().cpu().numpy()
     from robot.utils import write_video

robot/model/arm/envs/arm_controller.py
@@ -19,10 +19,10 @@ class Controller:
 
         state, goal = state['observation'], state['desired_goal']
 
-        assert len(state) == 29
+#         assert len(state) == 29
         qpos = state[:13]
         qvel = state[13:26]
-        achieved = state[26:29]
+        achieved = state[-3:]
 
         self.env.agent.set_qpos(qpos)
         self.env.agent.set_qvel(qvel)
@@ -31,12 +31,13 @@ class Controller:
 
         delta = np.linalg.lstsq(jac[:3], goal-achieved)[0] * 10 # desired_velocity
         q_delta = qvel.copy() * 0
-        q_delta[self.env._actuator_dof['agent']] = delta
+#         q_delta[self.env._actuator_dof['agent']] = delta
+        q_delta[1:8] = delta  # private attribute...
         qacc = (q_delta - qvel)/self.env.dt
 
-        qf = self.env.agent.compute_inverse_dynamics(qacc)[self.env._actuator_dof['agent']]
+        qf = self.env.agent.compute_inverse_dynamics(qacc)[1:8]
         self.env.load_state_vector(state_vector)
-        return qf/self.env._actuator_range['agent'][:, 1]
+        return qf/50  # again, private attribute...
 
 class RandomController:
     def __init__(self, env: ArmReachWithXYZ):

robot/model/arm/extra/lagrangian_v2.py
@@ -20,13 +20,15 @@ def get_batch_jacobian(net, x, noutputs):
     y,g = net(x)
     input_val = torch.eye(noutputs).reshape(1,noutputs, noutputs).repeat(n, 1, 1).cuda()
     x.retain_grad()
-    y.backward(input_val, retain_graph=True)
-    return y[:,0,:], g[:,0,:], x.grad.data
+    jac=torch.autograd.grad(y,x,input_val,create_graph=True)
+    return y[:,0,:], g[:,0,:], jac[0]
 
 
 def inverse_model(net_L, q, dq, ddq, return_all=False):
     nbatch=q.shape[0]
     ndim=q.shape[1]
+    dqr=dq.unsqueeze(1).repeat(1,ndim,1)
+    
     l, g, l_jac=get_batch_jacobian(net_L, q, int(ndim*(ndim+1)/2))
     L = torch.zeros((nbatch, ndim, ndim)).cuda()
     tril_indices = torch.tril_indices(row=ndim, col=ndim, offset=-1)
@@ -36,20 +38,46 @@ def inverse_model(net_L, q, dq, ddq, return_all=False):
     
     dLdq=torch.zeros((nbatch, ndim, ndim, ndim)).cuda()
     dLdq[:,tril_indices[0], tril_indices[1],:]=l_jac[:,:-ndim]
-    dLdq[:,torch.arange(ndim),torch.arange(ndim)]=l_jac[:,-ndim:]
-#    dLdt=(dLdq@dq.unsqueeze(2)).squeeze()
+    dLdq[:,torch.arange(ndim),torch.arange(ndim)]=l_jac[:,-ndim:]   
+    dLdt=(dLdq@dqr.unsqueeze(3)).squeeze()
     
-    dLdt=dLdq.view(nbatch,-1,ndim).bmm(dq.unsqueeze(2)).view(nbatch,ndim,ndim)
     H=L@L.transpose(1,2)
-    dHdt=L@dLdt.transpose(1,2)+dLdt@L.transpose(1,2)
-#     dHdq=dLdq.permute(0,3,1,2)@L.transpose(1,2)+L@dLdq.permute(0,3,2,1)
-    dHdq=dLdq.permute(0,3,1,2).reshape(nbatch,-1,ndim).bmm(L.transpose(1,2)).view(nbatch,ndim,ndim,ndim)
+    dHdt=L@dLdt.transpose(1,2)
+    dHdt=dHdt+dHdt.transpose(1,2)
+    dHdq=dLdq.permute(0,3,1,2)@(L.unsqueeze(1).repeat(1,ndim,1,1).transpose(2,3))
     dHdq=dHdq+dHdq.transpose(2,3)
-#     quad=((dq.unsqueeze(1)@dHdq)@dq.unsqueeze(2)).squeeze() # d(dqHdq)dq
-    quad=dq.unsqueeze(1).bmm(dHdq.view(nbatch,ndim,-1)).view(nbatch,-1,ndim).bmm(dq.unsqueeze(2)).squeeze()
-    tau=H.bmm(ddq.unsqueeze(2)).squeeze()+dHdt.bmm(dq.unsqueeze(2)).squeeze()-0.5*quad+g
+    
+    quad=(dqr.unsqueeze(2)@dHdq@dqr.unsqueeze(3)).squeeze()
+    tau=(H@ddq.unsqueeze(2)).squeeze()+(dHdt@dq.unsqueeze(2)).squeeze()-quad/2+g
     if return_all:
-        return L,dLdq,dLdt,H,dHdq,dHdt,tau
+        return L,dLdq,dLdt,H,dHdq,dHdt,quad,tau
+    else:
+        return tau
+    
+def inverse_model_v2(net_L, q, dq, ddq, return_all=False):
+    nbatch=q.shape[0]
+    ndim=q.shape[1]
+    dqr=dq.unsqueeze(1).repeat(1,ndim,1)
+    l, g, l_jac=get_batch_jacobian(net_L, q, int(ndim*(ndim+1)/2))
+    L = torch.zeros((nbatch, ndim, ndim)).cuda()
+    tril_indices = torch.tril_indices(row=ndim, col=ndim, offset=-1)
+
+    L[:,tril_indices[0], tril_indices[1]] = l[:,:-ndim]
+    L[:,torch.arange(ndim),torch.arange(ndim)]=l[:,-ndim:]
+
+    dLdq=torch.zeros((nbatch, ndim, ndim, ndim)).cuda()
+    dLdq[:,tril_indices[0], tril_indices[1],:]=l_jac[:,:-ndim]
+    dLdq[:,torch.arange(ndim),torch.arange(ndim)]=l_jac[:,-ndim:]
+
+    dLdt=(dLdq@dqr.unsqueeze(3)).squeeze()
+    H=L+L.transpose(1,2)
+    dHdt=dLdt+dLdt.transpose(-1,-2)
+    dHdq=dLdq+dLdq.transpose(-1,-2)
+    dHdq=dHdq.permute(0,3,1,2)
+    quad=(dqr.unsqueeze(2)@dHdq@dqr.unsqueeze(3)).squeeze()
+    tau=(H@ddq.unsqueeze(2)).squeeze()+(dHdt@dq.unsqueeze(2)).squeeze()-quad/2+g
+    if return_all:
+        return L,dLdq,dLdt,H,dHdq,dHdt,quad,tau
     else:
         return tau
     
@@ -57,9 +85,10 @@ class TestModule(nn.Module):
     # Test module only for ndim=3
     def __init__(self):
         super(TestModule, self).__init__()
+        self.g=nn.Linear(3,3)
         
     def forward(self, x):
-        return torch.cat([torch.exp(x),torch.exp(2*x)],-1)
+        return torch.cat([torch.exp(x),torch.exp(2*x)],-1), self.g(x)
 
 if __name__ == '__main__':
     model_L=TestModule().cuda()