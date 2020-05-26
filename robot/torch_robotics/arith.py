fixed contact gradient

robot/torch_robotics/arith.py
@@ -141,15 +141,16 @@ def trace(R):
 
 def projectSO3(R):
     # project a matrix into SO3, by 6d pose methods..
-    out = torch.zeros_like(R)
     a1, a2 = R[..., 0], R[..., 1]
     assert a1.norm(dim=-1).min() > 1e-15, "check init norm"
 
     b1 = normalize(a1)
     b2 = normalize(a2 - (a2 * b1).sum(dim=-1, keepdim=True) * b1)
-    b3 = torch.cross(b1, b2, dim=-1)
-    out[..., 0], out[..., 1], out[..., 2] = b1, b2, b3
-    return out
+    return torch.stack((b1, b2, torch.cross(b1, b2, dim=-1)), dim=-1)
+
+def projectSE3(T):
+    xx = projectSO3(T[:, :3, :3])
+    return Rp_to_trans(xx, T[:, :3, 3])
 
 
 def logSO3(R):

robot/torch_robotics/geometry/simplex.py
@@ -12,9 +12,8 @@ class RigidBody:
         self.c_ptr = c_ptr
 
     def set_pose(self, pose):
-        p = tr.Rp_to_trans(tr.projectSO3(pose[:, :3, :3]), pose[:,:3, 3])
-
-        self.pose = p #???? is this ok?
+        p = tr.projectSE3(pose)
+        self.pose = p
         self.c_ptr.set_pose(tr.tocpu(p))
 
     def get_pose(self):
@@ -124,14 +123,13 @@ class Simplex:
                         outs = []
                         for i, pose in zip(SHAPES, ctx.saved_tensors):
                             gradV = torch.tensor(i.c_ptr.grad, dtype=gradient.dtype, device=gradient.device)
-                            #mat = tr.expse3(tr.vec_to_se3(gradV * 0.0000001))
-                            #grad = (tr.dot(pose, mat) - pose)/0.0000001
-                            #assert (grad2 - grad).abs().max() < 1e-6, f"{grad} {grad2}, {(grad2-grad).abs().max()}"
-                            grad2 = tr.vec_to_se3(gradV)
+                            gradV[:,:3]/=2 # NOTE: vecy strange hack...div two as the project will count the grad twice?
+                            grad2 = tr.dot(pose, tr.vec_to_se3(gradV))
                             outs.append(grad2)
                         return tuple(outs)
 
                 func = Grad.apply
+                #self.shapes[0].pose.register_hook(lambda x:print('x', x))
                 normal_pos, dist = func(
                     *[i.pose for i in self.shapes]
                 )

robot/torch_robotics/geometry/test/test_simplex3.py
@@ -25,5 +25,50 @@ def test():
     print(center2.grad)
 
 
+def test_gradient():
+    sim = Simplex(0, compute_gradients=True)
+    center = tr.togpu([[1, 1, 0, 0],
+                       [0, 1, 0, 0],
+                       [1, 0, 1, 0],
+                       [0, 0, 0, 1]])[None, :]
+    size = tr.togpu([1, 1, 1])[None, :]
+
+
+    center.requires_grad = True
+    box = sim.box(center, size)
+
+    center2 = tr.togpu([0, 0, 0.8])[None,:]
+    center2.requires_grad = True
+    sphere = sim.sphere(center2, tr.togpu([0.5]))
+    center2 = tr.togpu([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0.9],
+                       [0, 0, 0, 1]])[None, :]
+    box2 = sim.box(center2, size*0.99)
+
+    shapes = [box, box2]
+    sim.collide(shapes, update=True)
+
+    (sim.dist.sum()+sim.pose.sum()).backward()
+    print(center.grad)
+    #jac = sim.sim.jacobian.reshape(4, 2, 2, 6, 7)
+    #jac = jac.mean(axis=2)
+
+    h = 0.01
+    for i in range(4):
+        for j in range(4):
+            center3 = center.clone()
+            center3[:, i, j] += h
+            box.set_pose(center3)
+            sim.update()
+            a = sim.dist.sum() + sim.pose.sum()
+            center3[:, i, j] -= 2*h
+            box.set_pose(center3)
+            sim.update()
+            b = sim.dist.sum() + sim.pose.sum()
+            print(float((a-b)/2/h), end=' ')
+        print('')
+
+
 if __name__ == '__main__':
-    test()
+    test_gradient()